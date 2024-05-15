1037303bf06abe50318dc002705bd1841d1bef45
MAYANK SHARMA <145884197+MAYANK12SHARMA@users.noreply.github.com>
2024-05-15 18:20:24 +0530

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/img2.png";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Sweet Alerts🎉",
     description:
       "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+    url: "https://sweetalert2.github.io/#download",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Images",
     description:
       "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+    url: "https://www.bing.com/images/search?q=Background+Full+HD+Classic&form=RESTAB&first=1",
   },
   {
-    title: "My Resume Site",
+    title: "Classic Hd Pictures",
     description:
       "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+    url: "https://www.bing.com/images/search?q=Background+Full+HD+Classic&form=RESTAB&first=1",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Github world",
     description:
       "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+    url: "https://www.youtube.com",
   },
 ];
 
