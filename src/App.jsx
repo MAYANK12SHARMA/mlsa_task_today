81c3e7732c7bebfeed5d3353cb6604194756eab6
MAYANK SHARMA <145884197+MAYANK12SHARMA@users.noreply.github.com>
2024-05-15 18:18:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Mayank Sharma",
+  title: "Web Designer & Django developer",
+  email: "mayank@example.com",
+  gitHub: "MLSA",
+  instagram: "MLSA",
+  linkedIn: "MAYANK SHARMA",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "MAYANK",
+  youTube: "MAYANK SHARMA",
 };
 
 const primaryColor = "#4E567E";
